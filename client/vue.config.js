a3f7587c453c0b89c9d5f5c54857cbdf955f89b7
Justin McKenzie <justin.mckenzie@mongodb.com>
2023-03-10 13:49:47 -0500

    use proxy for local dev on client

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000/',
+      }
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
